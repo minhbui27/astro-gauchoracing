57f7d93c61a1442966641926718473ac87450bdb
Minh Bui <ultraminh2709@gmail.com>
2022-08-10 01:05:08 -0700

    made some commits to add typescript to the project and added some ts to make the navbar responsive

---
+++ src/components/Nav.tsx
@@ -0,0 +1,144 @@
+import { css } from 'cypress/types/jquery'
+import * as React from 'react'
+import { useState } from 'react'
+const Nav = (): JSX.Element => {
+  const [menuOpen, setMenuOpen] = useState<boolean | null>(true)
+  console.log(menuOpen)
+  return (
+    <nav className='bg-gray-800'>
+      <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+        <div className='relative flex items-center justify-between h-16'>
+          <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
+            {/* <!-- Mobile menu button--> */}
+            <button
+              onClick={() => {
+                setMenuOpen(!menuOpen)
+              }}
+              type='button'
+              className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
+              aria-controls='mobile-menu'
+              aria-expanded='false'
+            >
+              <span className='sr-only'>Open main menu</span>
+              {/* <!-- */}
+              {/*   Icon when menu is closed. */}
+              {/**/}
+              {/*   Heroicon name: outline/menu */}
+              {/**/}
+              {/*   Menu open: "hidden", Menu closed: "block" */}
+              {/* --> */}
+              <svg
+                className={(menuOpen ? 'block' : 'hidden') + ' h-6 w-6'}
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth='2'
+                stroke='currentColor'
+                aria-hidden='true'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M4 6h16M4 12h16M4 18h16'
+                />
+              </svg>
+              {/* <!-- */}
+              {/*   Icon when menu is open. */}
+              {/**/}
+              {/*   Heroicon name: outline/x */}
+              {/**/}
+              <svg
+                className={(menuOpen ? 'hidden' : 'block') + ' h-6 w-6'}
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth='2'
+                stroke='currentColor'
+                aria-hidden='true'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M6 18L18 6M6 6l12 12'
+                />
+              </svg>
+            </button>
+          </div>
+          <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
+            <div className='flex-shrink-0 flex items-center'></div>
+            <div className='hidden sm:block sm:ml-6'>
+              <div className='flex space-x-4'>
+                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+                <a
+                  href='#'
+                  className='bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium'
+                  aria-current='page'
+                >
+                  Dashboard
+                </a>
+
+                <a
+                  href='#'
+                  className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+                >
+                  Team
+                </a>
+
+                <a
+                  href='#'
+                  className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+                >
+                  Projects
+                </a>
+
+                <a
+                  href='#'
+                  className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+                >
+                  Calendar
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+      <div className={(!menuOpen ? 'block sm:hidden' : 'hidden')}>
+        <div className='px-2 pt-2 pb-3 space-y-1'>
+          {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+          <a
+            href='#'
+            className='bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'
+            aria-current='page'
+          >
+            Dashboard
+          </a>
+
+          <a
+            href='#'
+            className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
+          >
+            Team
+          </a>
+
+          <a
+            href='#'
+            className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
+          >
+            Projects
+          </a>
+
+          <a
+            href='#'
+            className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
+          >
+            Calendar
+          </a>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
