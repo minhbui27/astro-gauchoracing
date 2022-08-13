3fba239164db7079ae72845bee3e40ac05c0812a
Minh Bui <ultraminh2709@gmail.com>
2022-08-12 22:45:36 -0700

    changed from fontawesome to styled-icons because of current incompatibility with astro

---
+++ src/components/Icons.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import {Instagram, Twitter} from '@styled-icons/boxicons-logos'
+import {Linkedin} from '@styled-icons/bootstrap'
+export function FooterIcons() {
+  return (
+    <div className='space-x-1 mt-2'>
+      <a href='https://www.instagram.com/sae.ucsb/'>
+		<Instagram size="36"/>
+      </a>
+      <a href='https://www.instagram.com/sae.ucsb/'>
+		<Twitter size="36"/>
+      </a>
+      <a href='https://www.instagram.com/sae.ucsb/'>
+		<Linkedin size="36"/>
+      </a>
+    </div>
+  )
+}
