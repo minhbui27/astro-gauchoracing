f310c169511cde403cb657a9c683f1874cbc1de5
Minh Bui <ultraminh2709@gmail.com>
2022-08-14 22:12:07 -0700

    finished the scrolling images in who we are section

--- src/components/home/HomeHelpers.tsx
+++ src/components/home/HomeHelpers.tsx
@@ -1,20 +1,32 @@
 import * as React from 'react'
 import { motion } from 'framer-motion'
 interface ImagesProps {
-  images?: string[]
+  images?: string[],
+  // direction 0 is to the left, 1 is to the right
+  direction?: number,
+  duration?: number
 }
-
+interface SideLinesProps {
+  repeat?: number
+}
 export default function ScrollingImages(props: ImagesProps): JSX.Element {
+  const initial = props.direction ? -700 : 0
+  const animate = props.direction ? 0 : -700
   return (
     <motion.div className='h-1/3 overflow-hidden w-full flex'>
-      {props.images.map((e: string): JSX.Element => {
+      {props.images.map((e: string,i: number): JSX.Element => {
         return (
-          <div className='flex-none shrink-0 w-[22rem] h-full'>
+          <div key={i} className='flex-none shrink-0 w-[350px] h-full'>
             <motion.img
-              // initial={{x: -100}}
-              //     animate={{ x: 200 }}
-              //     transition={{ ease:"linear", repeat: Infinity, repeatType: "reverse", duration: 2 }}
-              className='object-contain h-full w-full'
+              initial={{ x: initial}}
+              animate={{ x: animate}}
+              transition={{
+                ease: 'linear',
+                repeat: Infinity,
+                repeatType: 'loop',
+                duration: props.duration,
+              }}
+              className='py-2 object-contain h-full w-full'
               src={`/home_images/${e}.jpg`}
               alt={`${e}`}
             />
@@ -22,5 +34,15 @@ export default function ScrollingImages(props: ImagesProps): JSX.Element {
         )
       })}
     </motion.div>
+  )
+}
+
+export function SideLines(props: SideLinesProps): JSX.Element {
+  return (
+    <div className='flex flex-row overflow-hidden space-x-6 bg-white h-1/5 w-full'>
+		{Array(props.repeat).fill(0).map((e:number,i :number) => {
+			return (<div key={e+i} className='flex-none shrink-0 bg-red-600 h-full w-6'></div>)
+		})} 
+    </div>
   )
 }
