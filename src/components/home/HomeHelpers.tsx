9f7f0aae5e9fec6cf5dff29a43abae9d472d6076
Minh Bui <ultraminh2709@gmail.com>
2022-08-14 20:51:18 -0700

    rigged image animation for who we are div, and set up eslint

--- src/components/home/HomeHelpers.tsx
+++ src/components/home/HomeHelpers.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react'
+import { motion } from 'framer-motion'
 interface ImagesProps {
   images?: string[]
 }
+
 export default function ScrollingImages(props: ImagesProps): JSX.Element {
   return (
-    <div className="h-1/3 flex flex-row">
+    <motion.div className='h-1/3 overflow-hidden w-full flex'>
       {props.images.map((e: string): JSX.Element => {
-        return <img src={`/home_images/${e}.jpg`} alt={`${e}`} />
+        return (
+          <div className='flex-none shrink-0 w-[22rem] h-full'>
+            <motion.img
+              // initial={{x: -100}}
+              //     animate={{ x: 200 }}
+              //     transition={{ ease:"linear", repeat: Infinity, repeatType: "reverse", duration: 2 }}
+              className='object-contain h-full w-full'
+              src={`/home_images/${e}.jpg`}
+              alt={`${e}`}
+            />
+          </div>
+        )
       })}
-    </div>
+    </motion.div>
   )
 }
