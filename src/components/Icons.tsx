9d6aaa4c4d31360f0ca7e8b15ee2b3fc975d3288
Minh Bui <ultraminh2709@gmail.com>
2022-08-21 00:09:48 -0700

    fixed icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react'
 import {Instagram, Twitter} from '@styled-icons/boxicons-logos'
 import {Linkedin} from '@styled-icons/bootstrap'
-import styled from 'styled-components'
-import {ArrowheadDown} from '@styled-icons/evaicons-solid/ArrowheadDown'
-import {motion} from 'framer-motion'
-const ArrowDown = styled(ArrowheadDown)`
-	color:#003660;
-`
 export function FooterIcons() {
   return (
     <div className='space-x-1 mt-2'>
@@ -20,13 +14,5 @@ export function FooterIcons() {
 		<Linkedin size="36"/>
       </a>
     </div>
-  )
-}
-
-export function HomeIcon() {
-	return(
-		<motion.div animate={{y:50}} transition={{ease:"linear", repeat:Infinity, repeatType: "loop", duration: 2}}>
-			<ArrowDown size="48"/>
-		</motion.div>
 	)
 }
