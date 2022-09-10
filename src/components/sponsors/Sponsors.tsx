2aff7d833c8668ebe5fd022c4c697060d49e7516
Minh Bui <ultraminh2709@gmail.com>
2022-09-10 08:30:41 -0700

    improved responsiveness for the sponsors page

--- src/components/sponsors/Sponsors.tsx
+++ src/components/sponsors/Sponsors.tsx
@@ -12,10 +12,10 @@ const Sponsors = (): JSX.Element => {
         <div className='text-center justify-center text-4xl md:text-6xl lg:text-7xl flex flex-col our-sponsors h-32 w-full'>
           Our Sponsors
         </div>
-        <div className='px-12 md:px-20 lg:px-32 w-full'>
+        <div className='px-4 md:px-20 lg:px-32 w-full'>
           <img src={autodesk} alt='autodesk' />
         </div>
-        <div className='h-32 md:h-64 md:mt-16 px-8 flex flex-row md:px-16 lg:px-24 w-full'>
+        <div className='md:h-64 mt-8 md:mt-16 px-8 flex flex-col md:flex-row md:px-16 lg:px-24 w-full'>
           <div className='flex flex-row justify-center flex-1'>
             <img
               src={chassissim}
@@ -33,47 +33,37 @@ const Sponsors = (): JSX.Element => {
         <div className='text-center justify-center text-4xl md:text-6xl lg:text-7xl flex flex-col our-sponsors h-32 w-full'>
           Individual Donors
         </div>
-        <div className='px-32 flex flex-row'>
-          <div className='space-y-2 text-2xl flex-1 flex flex-col text-center justify-center'>
+        <div className='px-32 flex flex-row justify-center'>
+          <div className='space-y-2 text-2xl flex-1 flex flex-row justify-center'>
+			<div className='flex flex-col'>
             <p>Eli Bocek-Rivele</p>
-
             <p>Vin Tran</p>
-
             <p>Alen Iqbal</p>
-
             <p>Kelly Yan</p>
-
             <p>Mark Allen</p>
-
             <p>Charles Evans</p>
-
             <p>Vedant Shah</p>
+			</div>
           </div>
-          <div className='space-y-2 flex-1 text-2xl flex flex-col text-center justify-center'>
+          <div className='space-y-2 text-2xl flex-1 flex flex-row justify-center'>
+			<div className='flex flex-col'>
             <p>Andrew Peng</p>
-
             <p>Stephen Rivele</p>
-
             <p>Jennifer Hata</p>
-
             <p>Collin Dunner</p>
-
             <p>Dylan Pratt</p>
-
             <p>Owen Knudsen</p>
+			</div>
           </div>
-          <div className='space-y-2 flex-1 text-2xl flex flex-col text-center justify-center'>
+          <div className='space-y-2 text-2xl flex-1 flex flex-row justify-center'>
+			<div className='flex flex-col'>
             <p>Mark Allen</p>
-
             <p>Appan Thirumaligai</p>
-
             <p>Biao Chen</p>
-
             <p>Dan LeVier</p>
-
             <p>Stephen Wong</p>
-
             <p>Lisa Marchese</p>
+			</div>
           </div>
         </div>
       </div>
