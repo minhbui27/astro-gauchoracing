8ffe2ac28bb198cb83ac9c179f8492b3d226e444
Minh Bui <ultraminh2709@gmail.com>
2022-09-15 19:42:40 -0700

    test push

--- src/components/sponsors/SponsorSheets.ts
+++ src/components/sponsors/SponsorSheets.ts
@@ -2,11 +2,10 @@ import { google } from 'googleapis'
 export async function getServerSideProps(cellRange: number[]) {
     //getting the auth
     const auth = await google.auth.getClient({
-      keyFilename: './secrets.json',
+      credentials: JSON.parse(import.meta.env.GOOGLE_SHEETS_AUTHENTICATION),
       scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
     })
     const sheets = google.sheets({ version: 'v4', auth })
-
     //query
     const range = `Sheet1!A${cellRange[0]}:A${cellRange[1]}`
     const response = await sheets.spreadsheets.values.get({
