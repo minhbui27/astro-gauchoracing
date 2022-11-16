0afb7a2a0a9e9ab37a7c99dc9db9a2ac77467c74
Minh Bui <ultraminh2709@gmail.com>
2022-11-15 23:19:31 -0800

    added shop directory and corrected minor oopsie in sponsors page

---
+++ src/components/shop/Shop.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export default function Shop() {
+	return(
+		<div className='w-full h-screen'>
+			<div className='flex flex-col w-full h-full'>
+			</div>
+		</div>
+	)
+}
