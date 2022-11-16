1157cb0010fec8585d3a4c69ba32694859e9d683
Minh Bui <ultraminh2709@gmail.com>
2022-11-15 17:19:59 -0800

    changed the instagram and linkedin link because the old one is very outdate

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,13 +4,13 @@ import {Linkedin} from '@styled-icons/bootstrap'
 export function FooterIcons() {
   return (
     <div className='space-x-1 mt-2'>
-      <a href='https://www.instagram.com/sae.ucsb/'>
+      <a href='https://www.instagram.com/gauchoracingucsb/'>
 		<Instagram size="36"/>
       </a>
-      <a href='https://www.instagram.com/sae.ucsb/'>
+      <a href='https://www.instagram.com/gauchoracingucsb/'>
 		<Twitter size="36"/>
       </a>
-      <a href='https://www.instagram.com/sae.ucsb/'>
+      <a href='https://www.linkedin.com/company/gaucho-racing-at-uc-santa-barbara/mycompany/'>
 		<Linkedin size="36"/>
       </a>
     </div>
