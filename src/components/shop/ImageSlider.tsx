973272c9294793023d8fd075b2cb7c3686d9f1d8
Minh Bui <ultraminh2709@gmail.com>
2023-08-14 03:52:10 -0700

    made some progress with scrolling images for shop page

--- src/components/shop/ImageSlider.tsx
+++ src/components/shop/ImageSlider.tsx
@@ -1,30 +1,36 @@
 import * as React from 'react'
-import {motion} from 'framer-motion'
+import {motion, useAnimate} from 'framer-motion'
+import {ChevronRight} from '@styled-icons/bootstrap/ChevronRight'
 interface ImageProps {
 	imgList: string[]
 }
 // taking a list of image names as props
 export const ImageSlider = (props:ImageProps):JSX.Element => {
+	const [scope, animate] = useAnimate()
 	// setting current index and on button click update idx appropriately
 	const [currentIdx,setCurrentIdx] = React.useState<number | null>(0)
 	const imgList = props.imgList
 	// logic to move between the images, accessing via index
 	const handleNext = () => {
 		currentIdx + 1 >= props.imgList.length ? setCurrentIdx(0) : setCurrentIdx(currentIdx => currentIdx + 1)
+		animate(scope.current, {x: `-${currentIdx*100}px`})
 	}
 	const handlePrev = () => {
 		currentIdx - 1 < 0 ? setCurrentIdx(props.imgList.length - 1) : setCurrentIdx(currentIdx => currentIdx - 1)
+		animate(scope.current, {x: `${currentIdx*100}px`})
 	}
 	return(
 		<div>
 			<button onClick={handleNext}>next</button>
 			<button onClick={handlePrev}>prev</button>
-			<div className='overflow-hidden w-32 flex flex-row'>
-				{imgList.map((img,idx) => currentIdx === imgList.indexOf(`${img}`) ? 
-					<motion.img className='object-cover' animate={{x:`-${idx*100}%`, opacity: 1}} 
-						transition={{ease:'linear', duration: 0.5/idx,}}
-						key={idx} src={`/shop_images/${img}`}/> :
-					<motion.img animate={{x:`-${idx*100}%`, opacity: 0}} key={idx} src={`/shop_images/${img}`}/>) }
+			<div className='bg-cyan w-64 h-64 flex flex-row'>
+				<motion.div ref={scope} className='flex flex-row justify-start'>
+				{imgList.map((img,idx) => 
+						<div className='w-48'>
+						<img className='object-cover w-48 h-64' key={idx} src={`/shop_images/${img}`}/>
+						</div>
+				)}
+				</motion.div>
 			</div>
 		</div>
 	)
--- src/components/shop/Shop.tsx
+++ src/components/shop/Shop.tsx
@@ -5,7 +5,7 @@ export default function Shop(): JSX.Element {
   return (
     <div className='pt-20 w-full min-h-screen bg-shop'>
 			<div className='min-h-screen w-full flex-row'>
-				<div className='flex-1 p-8 min-h-screen w-1/3 bg-lightpink'>
+				<div className='flex-1 p-8 min-h-screen bg-lightpink'>
 					<ImageSlider imgList={['hoodie_1.jpg','hoodie_2.jpg','hoodie_3.jpg']}/>
 				</div>
 			</div>
